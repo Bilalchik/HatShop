a3592963835d0b5b5dd15bce9176f1004f44e32b
Bilalchik <azbk2004@gmail.com>
2024-08-20 05:53:25 +0000

    first commit on new branch for devops

--- core/core/settings.py
+++ core/core/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -126,8 +127,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_ROOT = 'media/'
+# Добавьте эту строку
+STATIC_ROOT = '/home/Bilal312/HatShop/core/staticfiles'
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
